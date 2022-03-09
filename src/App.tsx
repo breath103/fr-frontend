c21aa5bbd5b35b4f945e49c696f0e089aa53461e
Kurt Lee <breath103@gmail.com>
2022-03-09 23:58:19 +0900

    fix lint

--- src/App.tsx
+++ src/App.tsx
@@ -10,12 +10,8 @@ export const App: Component = () => {
     <Router>
       <div class="h-full flex flex-col bg-zinc-900 text-gray-100 overflow-overlay">
         <header class="bg-zinc-700 sticky top-0 p-2 text-center space-x-8">
-          <Link href="/boards/troll">
-            트롤촌
-          </Link>
-          <Link href="/boards/human">
-            선비촌
-          </Link>
+          <Link href="/boards/troll">트롤촌</Link>
+          <Link href="/boards/human">선비촌</Link>
         </header>
 
         <main class="flex-auto pt-4 pb-12">
